0ca4e65488aa661ed0d8c3354cbf18d487b45de1
root <ibrahimabdulqoyyum@gmail.com>
2021-03-20 19:45:50 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>PHP-CRUD</title>
+
+</head>
+<body>
+
+    <?php require_once 'process.php'; ?>
+
+    <?php if(isset($_SESSION['message'])): ?>
+
+        <div class="alert alert-<?php echo $_SESSION['alert']; ?>"> 
+
+            <?php 
+
+                echo $_SESSION['message'];
+                unset($_SESSION['message']);
+
+            ?>
+
+        </div>
+        
+    <?php endif; ?>
+
+<div class="container">
+    <?php
+
+        $mysqli = new mysqli('localhost', 'root', 'admin@admin.com', 'crud') or die(mysqli_error($mysqli));
+
+        $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
+
+    ?>
+
+    <div class="row justify-content-center">
+    <h1>PHP-CRUD SYSTEM</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th colspan="2">Action</th>
+                </tr>
+            
+            </thead>
+        <?php while ($row = $result->fetch_array()): ?>
+
+            <tr>
+                <td><?php echo $row['name']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td>
+                    <a href="index.php?edit=<?php echo $row['id']; ?>" class="btn btn-info">Edit</a>
+                    <a href="process.php?delete=<?php echo $row['id'];?>" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+
+        <?php endwhile; ?>
+
+        </table>
+    
+    </div>
+
+
+    <div class="row justify-content-center">
+
+        <form action="process.php" method="POST"> 
+            <input type="hidden" name="id" value="<?php echo $id ?>;">
+        <div class="form-group">
+
+        <label for="name">Name </label>
+        <input type="text" name="name" placeholder="Enter your name"  value="<?php echo $name; ?>" class="form-control" required><br>
+        </div>
+
+        <div class="form-group">
+        <label for="Email">Email </label>
+        <input type="text" name="email" placeholder="Enter your email" value="<?php echo $email; ?>" class="form-control" required><br>
+        </div>
+
+
+        <div class="form-group">
+        <?php  if($update == true): ?>
+            <button type="submit" name="update" class="btn btn-success">Update</button>
+        </div>
+
+        <?php else: ?>
+            <button type="submit" name="submit" class="btn btn-primary">Save</button>
+        
+        <?php endif; ?>
+       </div>
+        </form>
+    
+    </div>
+
+</div>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,63 @@
+<?php
+
+
+session_start();
+
+$update = false;
+$id = 0;
+$name = "";
+$email = "";
+
+$mysqli = new mysqli('localhost', 'root', 'admin@admin.com', 'crud') or die(mysqli_error($myqli));
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+
+    $mysqli->query("INSERT INTO data (name, email) VALUES('$name', '$email')") or die("Error while querying database");
+
+    $_SESSION['message'] = "You've added your record";
+    $_SESSION['alert'] = "success";
+
+    header("location: index.php");
+}
+
+if (isset($_GET['delete'])){
+
+    $id = $_GET['delete'];
+
+    $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error());
+
+    $_SESSION['message'] = "Your record has been deleted";
+    $_SESSION['alert'] = "danger";
+    
+    header("location: index.php");
+}
+
+if(isset($_GET['edit'])){
+    
+    $id = $_GET['edit'];
+    $update = true;
+    $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die("Error querying database");
+
+    if(!empty($result)){
+        $row = $result->fetch_array();
+        $name = $row['name'];
+        $email = $row['email'];
+    }
+}
+
+if(isset($_POST['update'])){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $mysqli->query("UPDATE data SET name='$name', email='$email' WHERE id=$id") or die("Error while querying database");
+
+    $_SESSION['message'] = "Your record has been updated";
+    $_SESSION['alert'] = "success";
+
+    header("location: index.php");
+}
+?>
